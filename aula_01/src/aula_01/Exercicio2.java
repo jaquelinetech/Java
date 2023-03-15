eb6294fc78f1ef67a5ba2136be79376e4dadd556
jaqueline oliveira <jaqueline.3010@yahoo.com.br>
2023-03-15 19:20:57 -0300

    Exercicios Java

--- aula_01/src/aula_01/Exercicio2.java
+++ aula_01/src/aula_01/Exercicio2.java
@@ -1,10 +1,33 @@
 package aula_01;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+          
+		Scanner leia = new Scanner(System.in);
+		
+		float n1, n2, n3, n4, media;
+		
+		System.out.println("Digite nota 1:");
+		n1= leia.nextFloat();
+		System.out.println("Digite nota 2:");
+		n2= leia.nextFloat();
+		System.out.println("Digite nota 3:");
+		n3= leia.nextFloat();
+		System.out.println("Digite nota 4:");
+		n4= leia.nextFloat();
+		
+		media = (n1 + n2 + n3 + n4)/4;
+		
+		System.out.println("A media é: " + media);
+		
+		leia.close();
+		
+		
+		
 	}
 
 }
--- aula_01/src/aula_01/Exercicio4.java
+++ aula_01/src/aula_01/Exercicio4.java
@@ -1,5 +1,30 @@
 package aula_01;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner leia = new Scanner (System.in);
+        DecimalFormat df = new DecimalFormat("###,###,##0,00");
+        
+        float n1,n2,n3,n4;
+        
+
+        System.out.println("Digite a nota 1:");
+        n1=leia.nextFloat();
+        System.out.println("Digite a nota 2:");
+        n2=leia.nextFloat();
+        System.out.println("Digite a nota 3:");
+        n3=leia.nextFloat();
+        System.out.println("Digite a nota 4:");
+        n4=leia.nextFloat();
+        
+        System.out.println("A Diferença é:" + ((n1 * n2) - (n3 * n4)));
+        
+			
+	}
 
 }
