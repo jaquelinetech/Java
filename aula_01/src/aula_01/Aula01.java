81e1ef69d62a7ebe4ffe7b5ed0b55f679cddc35c
jaqueline oliveira <jaqueline.3010@yahoo.com.br>
2023-03-20 18:14:07 -0300

    Exercicios

--- aula_01/src/aula_01/Aula01.java
+++ aula_01/src/aula_01/Aula01.java
@@ -18,13 +18,15 @@ public class Aula01 {
 		System.out.println("Digite o primeiro numero:");
 		double numero1 = leia.nextDouble();
 		
-		System.out.println("Digite o segundo numero");
+		System.out.println("Digite o 1 numero");
 		numero2 = leia.nextDouble();
 		
 		System.out.println("Soma:"+ df.format((numero1 + numero2)));
 		System.out.println("Subtração:"+ df.format((numero1- numero2)));
 		System.out.println("Multiplicação:" + (numero1* numero2));
+		if (numero1 != 0 && numero2 != 0) {
 		System.out.println("Divisão:"+ df.format((numero1/ numero2)));
+		}
 		System.out.println("Potencia:" + df.format(Math.pow(numero1, numero2)));
 		System.out.println("Raiz quadrada:" + df.format(Math.sqrt(numero1)));
 		
